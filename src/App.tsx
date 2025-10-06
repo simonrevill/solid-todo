35b855021fd8c987aff12861105d1c62f39bc47f
simonrevill <midlandmusic@googlemail.com>
2025-10-06 09:11:06 +0100

    feat: add form element to App component and corresponding test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 function App() {
-  return <h1>Solid Todo</h1>;
+  return (
+    <>
+      <h1>Solid Todo</h1>
+      <form aria-label="todo-form"></form>
+    </>
+  );
 }
 
 export default App;
--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -14,4 +14,15 @@ describe("rendering", () => {
     // Assert
     expect(heading).toBeVisible();
   });
+
+  it("renders form for inputting todos", () => {
+    // Arrange
+    render(() => <App />);
+    const form = screen.getByRole("form", {
+      name: /todo-form/i,
+    });
+
+    // Assert
+    expect(form).toBeVisible();
+  });
 });
